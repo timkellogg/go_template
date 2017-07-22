932d7ef12c181ee14b4abf4aa56823604fb1accd
timkellogg <tim.kellogg@gmail.com>
2017-07-22 10:40:24 -0700

    refactor user model struct to remove dup fields

--- models/user.go
+++ models/user.go
@@ -1,16 +1,11 @@
 package models
 
 import (
-	"time"
-
 	"github.com/jinzhu/gorm"
 )
 
 // User model
 type User struct {
 	gorm.Model
-	ID        int64     `db:"id"`
-	email     string    `db:"email"`
-	CreatedAt time.Time `db:"created_at"`
-	UpdatedAt time.Time `db:"updated_at"`
+	email string `db:"email"`
 }
